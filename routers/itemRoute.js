8120f093eb2a1638d7efaa7702328e788254b2b4
JuzzThyne <justinepodiotan19@gmail.com>
2024-01-12 20:54:03 +0800

    added add,update, delete

--- routers/itemRoute.js
+++ routers/itemRoute.js
@@ -35,8 +35,56 @@ const verifyToken = (req, res, next) => {
     }
 };
 
+// routes for fetching all items
+router.post('/', verifyToken, async(req , res) => {
+    try{
+        const { searchterm, page = 1, limit = 10, sortOrder = 'asc'} = req.body;
+
+        const query = searchterm
+        ? {
+            $or: [
+                {itemName: { $regex: new RegExp(searchterm, 'i')}},
+            ],
+          }
+        : {};
+
+        // Perform a count query to get the total number of users
+      const totalItemCount = await Item.countDocuments(query);
+  
+      const skip = (page - 1) * limit;
+  
+      const sortOption = sortOrder === 'asc' ? { itemName: 1 } : { itemName: -1 };
+  
+      const items = await Item.find(query)
+        .skip(skip)
+        .limit(limit)
+        .sort(sortOption); // Sort by itemName
+  
+
+      // Map the items array to create an array of filtered items
+        const filteredItems = items.map(item => ({
+            itemId: item._id,
+            itemPhotoUrl: item.itemPhotoUrl,
+            itemName: item.itemName,
+            itemPrice: item.itemPrice,
+        }));
+
+      return res.status(200).json({
+        count: items.length,
+        currentPage: page,
+        totalPages: Math.ceil(totalItemCount / limit),
+        data: filteredItems,
+      });
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
 // routes for add items
-router.post('/', verifyToken, async (req, res) => {
+router.post('/add', verifyToken, async (req, res) => {
     try {
         const item = req.body;
 
@@ -45,7 +93,7 @@ router.post('/', verifyToken, async (req, res) => {
         });
 
         if (existingItem) {
-            res.status(400).send({ message: 'Item already exists!' });
+            res.status(400).send({ message: 'Item already exists!' , success: false });
             return;
         }
 
@@ -59,12 +107,88 @@ router.post('/', verifyToken, async (req, res) => {
         };
 
         await Item.create(newItem);
-        res.status(201).json({ message: 'Item added successfully!' });
+        res.status(201).json({ message: 'Item added successfully!' , success: true });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 });
+
+// Route for fetching a single item
+router.get('/:itemId', verifyToken, async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+        const item = await Item.findById(itemId);
+
+        if (!item) {
+            return res.status(404).json({ message: "Item not found" , success: false });
+        }
+        const decodedItem = item._id;
+
+        // Compare the userId with the decodedUser _id
+        if (itemId !== decodedItem.toString()) {
+            return res.status(403).json({ message: "Unauthorized access" , success: false });
+        }
+
+        // Create a new object with limited data
+        const limitedItemData = {
+            itemPhotoUrl: item.itemPhotoUrl,
+            itemName: item.itemName,
+            itemPrice: item.itemPrice,   
+        };
+
+        return res.status(200).json({ data: limitedItemData });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+// Route for updating a single item
+router.put('/:itemId', verifyToken, async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+        
+        const checkItem = await Item.findById(itemId);
+
+        const updatedItem = {
+            itemPhotoUrl: req.body.itemPhotoUrl || checkItem.itemPhotoUrl,
+            itemName: req.body.itemName || checkItem.itemName,
+            itemPrice: req.body.itemPrice || checkItem.itemPrice,
+        };
+
+        const item = await Item.findByIdAndUpdate(itemId, updatedItem, { new: true });
+
+        if (!item) {
+            return res.status(404).json({ message: "Item not found" , success: false });
+        }
+
+        return res.status(200).json({ message: 'Item updated successfully' , success: true });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+// Route for deleting a single user
+router.delete('/:itemId', verifyToken, async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+
+        const item = await Item.findByIdAndDelete(itemId);
+
+        if (!item) {
+            return res.status(404).json({ message: "Item not found", success: false });
+        }
+
+        return res.status(200).json({ message: 'Item deleted successfully' , success: true });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+
 
 
 
